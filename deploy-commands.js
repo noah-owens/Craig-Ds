f0cc80dd39a4fcd184d886fbaf7b248aca5c2868
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2022-08-09 10:19:07 -0600

    :bulb: Add commenting

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,18 +4,21 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
+// Create array of command.js files using a platform agnostic filepath
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '10' }).setToken(token);
-
+// Push JSON to commands [] (again, platform agnostic filepath)
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	commands.push(command.data.toJSON());
 }
 
+const rest = new REST({ version: '10' }).setToken(token);
+
+// Register commands with Guild scope
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
