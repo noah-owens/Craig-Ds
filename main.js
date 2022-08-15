064bd1734274ba9d228330cc2d881df7cad292da
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2022-08-15 12:57:51 -0600

    :bulb: Add and disable Player from discord-player

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
+// const { Player } = require('discord-player');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
 
 // Command Collector
 client.commands = new Collection();
@@ -47,5 +48,8 @@ for (const file of eventFiles) {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
 }
+
+// Create Player
+// const player = new Player(client);
 
 client.login(token);
