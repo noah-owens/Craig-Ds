bed0221331f11b53a3b90850273ab25a99f0ef59
Noah Owens <72886843+noah-owens@users.noreply.github.com>
2022-08-15 12:37:04 -0600

    :ambulance: Fix commandhandler

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,12 +4,10 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
-// Create array of command.js files using a platform agnostic filepath
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-// Push JSON to commands [] (again, platform agnostic filepath)
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
@@ -19,18 +17,6 @@ for (const file of commandFiles) {
 const rest = new REST({ version: '10' }).setToken(token);
 
 // Register slash commands with guild scope
-(async () => {
-	try {
-		console.log('Started refreshing (/) commands.');
-
-		await rest.put(
-			Routes.applicationGuildCommand(clientId, guildId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded (/) commands');
-	}
-	catch (error) {
-		console.error(error);
-	}
-});
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Command Handler
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// Command Collector
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
@@ -15,6 +15,23 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
+
+// Command Handler
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
 
 // Event Listener
 const eventsPath = path.join(__dirname, 'events');
